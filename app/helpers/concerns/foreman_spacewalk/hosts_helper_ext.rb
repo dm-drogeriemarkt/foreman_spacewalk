725de0a78569bcd9fed49f44248a79bfa5941d6b
Timo Goebel <mail@timogoebel.name>
2018-04-05 17:24:46 +0200

    support rails 5.1

--- app/helpers/concerns/foreman_spacewalk/hosts_helper_ext.rb
+++ app/helpers/concerns/foreman_spacewalk/hosts_helper_ext.rb
@@ -2,18 +2,20 @@ module ForemanSpacewalk
   module HostsHelperExt
     extend ActiveSupport::Concern
 
-    included do
-      alias_method_chain :multiple_actions, :spacewalk
+    module Overrides
+      def multiple_actions
+        spacewalk_multiple_actions = []
+        if authorized_for(controller: :hosts, action: :select_multiple_downtime)
+          spacewalk_multiple_actions << [
+            _('Change Spacewalk Proxy'), select_multiple_spacewalk_proxy_hosts_path
+          ]
+        end
+        super + spacewalk_multiple_actions
+      end
     end
 
-    def multiple_actions_with_spacewalk
-      spacewalk_multiple_actions = []
-      if authorized_for(controller: :hosts, action: :select_multiple_downtime)
-        spacewalk_multiple_actions << [
-          _('Change Spacewalk Proxy'), select_multiple_spacewalk_proxy_hosts_path
-        ]
-      end
-      multiple_actions_without_spacewalk + spacewalk_multiple_actions
+    included do
+      prepend Overrides
     end
   end
 end
